3a5665a0d7e2fcd47ec2db0556637e34b69898e5
AlejandroE <alejandroescobar717@gmail.com>
2023-10-11 16:59:12 -0500

    Registro del usuario

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
@@ -11,7 +12,15 @@ TIPO_EVENTO = [
 
 ]
 
-class Usuario(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
 
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
@@ -20,10 +29,12 @@ class Usuario(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     foto = models.CharField(max_length=250)
     is_active = models.BooleanField(default=True)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
 
 class Evento(models.Model):
 
-    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='creador_evento')
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=250)
     tipo = models.CharField(max_length=1,choices=TIPO_EVENTO)
@@ -31,16 +42,16 @@ class Evento(models.Model):
     terminado = models.BooleanField(default=False)
 
 class Actividad(models.Model):
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
-    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='evento_actividad')
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='creador_actividad')
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=250)
     valor = models.FloatField()
 
 class Contacto(models.Model):
 
-    usuario1 = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    usuario2 = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario1 = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='usuario1')
+    usuario2 = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='usuario2')
     is_active = models.BooleanField(default=True)
     
     class Meta:
@@ -49,27 +60,27 @@ class Contacto(models.Model):
 
 class Pago (models.Model):
 
-    deudor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    prestador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    deudor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='deudor_pago')
+    prestador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='prestador_pago')
     valor = models.FloatField()
 
 class UsuarioParticipaEvento(models.Model):
 
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
-    participante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='evento_participa')
+    participante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='participante_evento')
     is_active = models.BooleanField(default=True)
 
 class UsuarioParticipaActividad(models.Model):
 
-    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE)
-    participante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE, related_name='actividad_participa')
+    participante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='participante_actividad')
     valor = models.FloatField()
     is_active = models.BooleanField(default=True)
 
 class Invitacion(models.Model):
 
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='invitacion_evento')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='usuario_invitacion')
     aceptado =  models.BooleanField(default=False)
 
 
